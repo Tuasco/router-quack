13dfe2ff53df54eff6dcca447780ff102e7882d4
Tuasco <ktata.yacine@gmail.com>
2026-02-08 17:31:58 +0100

    Fix ErrorCollector.cs warnings.
    Roslyn has issue with methods taking params with the new extension syntax.

--- src/RouterQuack.Core/Extensions/ErrorCollector.cs
+++ src/RouterQuack.Core/Extensions/ErrorCollector.cs
@@ -2,21 +2,36 @@ using JetBrains.Annotations;
 using Microsoft.Extensions.Logging;
 using RouterQuack.Core.Steps;
 
+#pragma warning disable CA2254
+// ReSharper disable ConvertToExtensionBlock
+// Roslyn has know issues with params in extension block. See https://github.com/dotnet/roslyn/issues/80024.
+
 namespace RouterQuack.Core.Extensions;
 
 public static class ErrorCollectorExtensions
 {
-    extension(IErrorCollector source)
+    /// <summary>
+    /// Log an error and set ErrorsOccurred to true.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="message">Message as a structured template.</param>
+    /// <param name="args">Arguments to fill the message with.</param>
+    public static void LogError(this IErrorCollector source,
+        [StructuredMessageTemplate] string message, params object?[] args)
     {
-        public void LogError([StructuredMessageTemplate] string message, params object?[] args)
-        {
-            source.Logger.LogError(message, args);
-            source.ErrorsOccurred = true;
-        }
+        source.Logger.LogError(message, args);
+        source.ErrorsOccurred = true;
+    }
 
-        public void LogWarning([StructuredMessageTemplate] string message, params object?[] args)
-        {
-            source.Logger.LogWarning(message, args);
-        }
+    /// <summary>
+    /// Log a warning.
+    /// </summary>
+    /// <param name="source"></param>
+    /// <param name="message">Message as a structured template.</param>
+    /// <param name="args">Arguments to fill the message with.</param>
+    public static void LogWarning(this IErrorCollector source,
+        [StructuredMessageTemplate] string message, params object?[] args)
+    {
+        source.Logger.LogWarning(message, args);
     }
 }
