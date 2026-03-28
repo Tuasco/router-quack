d42be3db8f940a900301c46e343606e9904d2665
Tuasco <ktata.yacine@gmail.com>
2026-03-28 20:56:05 +0100

    Update tests
    The processor will always get fixed length neighbour paths since the responsibility shifted from the processor to the parsers

--- tests/RouterQuack.Tests/Unit/Core/Processors/ResolveNeighboursTests.cs
+++ tests/RouterQuack.Tests/Unit/Core/Processors/ResolveNeighboursTests.cs
@@ -13,14 +13,14 @@ public class ResolveNeighboursTests
     [Test]
     public async Task Process_ShortSameAsNeighbourWithoutConflict_ResolvesNeighbour()
     {
-        var r1Interface = CreatePendingInterface("GigabitEthernet2/0", "R3");
-        var r3Interface = CreatePendingInterface("GigabitEthernet1/0", "R1");
+        var r1Interface = CreatePendingInterface("GigabitEthernet1/0", "111:R2:");
+        var r2Interface = CreatePendingInterface("GigabitEthernet1/0", "111:R1:");
 
         var context = CreateContext(
             TestData.CreateAs(number: 111, routers:
             [
                 TestData.CreateRouter(name: "R1", interfaces: [r1Interface]),
-                TestData.CreateRouter(name: "R3", interfaces: [r3Interface])
+                TestData.CreateRouter(name: "R2", interfaces: [r2Interface])
             ]));
 
         var processor = new ResolveNeighbours(_logger, context);
@@ -28,23 +28,23 @@ public class ResolveNeighboursTests
         processor.Process();
 
         await Assert.That(processor.Context.ErrorsOccurred).IsFalse();
-        await Assert.That(r1Interface.Neighbour).IsEqualTo(r3Interface);
-        await Assert.That(r3Interface.Neighbour).IsEqualTo(r1Interface);
+        await Assert.That(r1Interface.Neighbour).IsEqualTo(r2Interface);
+        await Assert.That(r2Interface.Neighbour).IsEqualTo(r1Interface);
     }
 
     [Test]
     public async Task Process_ShortNeighbourWithParallelLinks_SetsErrorsOccurred()
     {
-        var r1Interface1 = CreatePendingInterface("GigabitEthernet2/0", "R3");
-        var r1Interface2 = CreatePendingInterface("GigabitEthernet3/0", "R3");
-        var r3Interface1 = CreatePendingInterface("GigabitEthernet1/0", "R1");
-        var r3Interface2 = CreatePendingInterface("GigabitEthernet2/0", "R1");
+        var r1Interface1 = CreatePendingInterface("GigabitEthernet1/0", "111:R2:");
+        var r1Interface2 = CreatePendingInterface("GigabitEthernet2/0", "111:R2:");
+        var r3Interface1 = CreatePendingInterface("GigabitEthernet1/0", "111:R1:");
+        var r3Interface2 = CreatePendingInterface("GigabitEthernet2/0", "111:R1:");
 
         var context = CreateContext(
             TestData.CreateAs(number: 111, routers:
             [
                 TestData.CreateRouter(name: "R1", interfaces: [r1Interface1, r1Interface2]),
-                TestData.CreateRouter(name: "R3", interfaces: [r3Interface1, r3Interface2])
+                TestData.CreateRouter(name: "R2", interfaces: [r3Interface1, r3Interface2])
             ]));
 
         var processor = new ResolveNeighbours(_logger, context);
@@ -61,16 +61,16 @@ public class ResolveNeighboursTests
     [Test]
     public async Task Process_ExplicitSameAsNeighbourWithParallelLinks_ResolvesEachInterfaceToUniquePeer()
     {
-        var r1Interface1 = CreatePendingInterface("GigabitEthernet2/0", "R3:GigabitEthernet1/0");
-        var r1Interface2 = CreatePendingInterface("GigabitEthernet3/0", "R3:GigabitEthernet2/0");
-        var r3Interface1 = CreatePendingInterface("GigabitEthernet1/0", "R1:GigabitEthernet2/0");
-        var r3Interface2 = CreatePendingInterface("GigabitEthernet2/0", "R1:GigabitEthernet3/0");
+        var r1Interface1 = CreatePendingInterface("GigabitEthernet1/0", "111:R2:GigabitEthernet1/0");
+        var r1Interface2 = CreatePendingInterface("GigabitEthernet2/0", "111:R2:GigabitEthernet2/0");
+        var r2Interface1 = CreatePendingInterface("GigabitEthernet1/0", "111:R1:GigabitEthernet1/0");
+        var r2Interface2 = CreatePendingInterface("GigabitEthernet2/0", "111:R1:GigabitEthernet2/0");
 
         var context = CreateContext(
             TestData.CreateAs(number: 111, routers:
             [
                 TestData.CreateRouter(name: "R1", interfaces: [r1Interface1, r1Interface2]),
-                TestData.CreateRouter(name: "R3", interfaces: [r3Interface1, r3Interface2])
+                TestData.CreateRouter(name: "R2", interfaces: [r2Interface1, r2Interface2])
             ]));
 
         var processor = new ResolveNeighbours(_logger, context);
@@ -78,21 +78,47 @@ public class ResolveNeighboursTests
         processor.Process();
 
         await Assert.That(processor.Context.ErrorsOccurred).IsFalse();
-        await Assert.That(r1Interface1.Neighbour).IsEqualTo(r3Interface1);
-        await Assert.That(r1Interface2.Neighbour).IsEqualTo(r3Interface2);
-        await Assert.That(r3Interface1.Neighbour).IsEqualTo(r1Interface1);
-        await Assert.That(r3Interface2.Neighbour).IsEqualTo(r1Interface2);
+        await Assert.That(r1Interface1.Neighbour).IsEqualTo(r2Interface1);
+        await Assert.That(r1Interface2.Neighbour).IsEqualTo(r2Interface2);
+        await Assert.That(r2Interface1.Neighbour).IsEqualTo(r1Interface1);
+        await Assert.That(r2Interface2.Neighbour).IsEqualTo(r1Interface2);
+    }
+
+    [Test]
+    public async Task Process_MinimalSameAsNeighbourWithParallelLinks_ResolvesEachInterfaceToUniquePeer()
+    {
+        var r1Interface1 = CreatePendingInterface("GigabitEthernet1/0", "111:R2:GigabitEthernet1/0");
+        var r1Interface2 = CreatePendingInterface("GigabitEthernet2/0", "111:R2:");
+        var r2Interface1 = CreatePendingInterface("GigabitEthernet1/0", "111:R1:");
+        var r2Interface2 = CreatePendingInterface("GigabitEthernet2/0", "111:R1:");
+
+        var context = CreateContext(
+            TestData.CreateAs(number: 111, routers:
+            [
+                TestData.CreateRouter(name: "R1", interfaces: [r1Interface1, r1Interface2]),
+                TestData.CreateRouter(name: "R2", interfaces: [r2Interface1, r2Interface2])
+            ]));
+
+        var processor = new ResolveNeighbours(_logger, context);
+
+        processor.Process();
+
+        await Assert.That(processor.Context.ErrorsOccurred).IsFalse();
+        await Assert.That(r1Interface1.Neighbour).IsEqualTo(r2Interface1);
+        await Assert.That(r1Interface2.Neighbour).IsEqualTo(r2Interface2);
+        await Assert.That(r2Interface1.Neighbour).IsEqualTo(r1Interface1);
+        await Assert.That(r2Interface2.Neighbour).IsEqualTo(r1Interface2);
     }
 
     [Test]
     public async Task Process_ExplicitCrossAsNeighbour_ResolvesNeighbour()
     {
-        var r1Interface = CreatePendingInterface("GigabitEthernet1/0", "222:R2:GigabitEthernet1/0");
+        var r1Interface = CreatePendingInterface("GigabitEthernet1/0", "112:R2:GigabitEthernet1/0");
         var r2Interface = CreatePendingInterface("GigabitEthernet1/0", "111:R1:GigabitEthernet1/0");
 
         var context = CreateContext(
             TestData.CreateAs(number: 111, routers: [TestData.CreateRouter(name: "R1", interfaces: [r1Interface])]),
-            TestData.CreateAs(number: 222, routers: [TestData.CreateRouter(name: "R2", interfaces: [r2Interface])]));
+            TestData.CreateAs(number: 112, routers: [TestData.CreateRouter(name: "R2", interfaces: [r2Interface])]));
 
         var processor = new ResolveNeighbours(_logger, context);
 
